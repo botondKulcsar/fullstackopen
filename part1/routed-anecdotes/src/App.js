c0e6614c598e5a97db0ad06b7de9fed976111978
Botond Kulcsar <bkulcsar@gmx.com>
2021-10-27 11:15:09 +0300

    exercise 7.4

--- part1/routed-anecdotes/src/App.js
+++ part1/routed-anecdotes/src/App.js
@@ -8,6 +8,8 @@ import {
   useHistory
 } from 'react-router-dom'
 
+import { useField } from './hooks'
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
@@ -64,17 +66,20 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  // const [content, setContent] = useState('')
+  // const [author, setAuthor] = useState('')
+  // const [info, setInfo] = useState('')
+  const { value: content, onChange: contentChange} = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
       content,
-      author,
-      info,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
   }
@@ -85,15 +90,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content} onChange={contentChange} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author.value} onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input name='info' value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
       </form>
